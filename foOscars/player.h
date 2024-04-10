8c1e00dc1716f1a875384af913bd2e6050fe6ad6
victoriayo2002 <victoriayo1335@gmail.com>
2024-04-10 17:05:59 -0600

    started player class

---
+++ foOscars/player.h
@@ -0,0 +1,25 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+#include <QVector>
+#include <QString>
+
+class Player
+{
+public:
+    Player();
+    std::vector<int>playerActionPile;
+    std::vector<std::vector<int>>playerPeoplePile;
+    int score;
+    int selectedActionIndex;
+    QString name;
+    int scoreManipulators[5];
+    int purpleCardSum;
+    int redCardSum;
+    int blueCardSum;
+    int greenCardSum;
+
+
+    void recalculateScore();
+};
+
+#endif // PLAYER_H
